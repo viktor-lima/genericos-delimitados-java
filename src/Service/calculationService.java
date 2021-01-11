ae31ad08624edd7f1280398714f4bde38e3eca72
viktor-lima <joaolimak10@outlook.com>
2021-01-11 18:34:50 -0300

    coringa type

--- src/Service/calculationService.java
+++ src/Service/calculationService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class calculationService {
 
+	//? coringa type list
 	public static <T extends Comparable<? super T>> T max(List<T> list) {
 		if (list.isEmpty()) {
 			throw new IllegalStateException("list can't be empty");
